c94aa791dbdf414a32e2190369f5905108bed10a
Michael-Yongshi <39827427+Michael-Yongshi@users.noreply.github.com>
2021-01-30 13:44:51 +0100

    added sun listener

---
+++ listeners/listen_sun.py
@@ -0,0 +1,28 @@
+import hassapi as hass
+
+class ListenSun(hass.Hass):
+    """
+    Type of class:
+    - Service Listener
+
+    Method called:
+    - Print to log: "Hey, Listen!"
+
+    Method is called whenever the sun rises or sets (which is a built-in event trigger)
+    """
+
+    def initialize(self):
+
+        # we register two seperate callbacks, one for sunrise and one for sunset.
+        self.run_at_sunrise(self.log_rise)
+        self.run_at_sunset(self.log_set, offset=-900)
+
+    def log_rise(self, kwargs):
+
+        self.log(f"Hey, Listen!")
+        self.log(f"Sun is now rising!")
+
+    def log_set(self, kwargs):
+
+        self.log(f"Hey, Listen!")
+        self.log(f"Sun is now setting!")
