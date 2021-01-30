84a2060a811f5dddc4196ab54d5a78b3dae9789c
Michael-Yongshi <39827427+Michael-Yongshi@users.noreply.github.com>
2021-01-30 11:24:41 +0100

    generic scripts

---
+++ generic/change_state.py
@@ -0,0 +1,34 @@
+
+# The way you change the state of an entity in AppDaemon is the same as in Home Assistant: you call a service. 
+# Changing the state of the entity directly does nothing but change the value in software! 
+# You must use the service to physically change the state of the device. 
+
+import appdaemon.plugins.hass.hassapi as hass
+
+class ListenState(hass.Hass):
+    """
+    Type of class:
+    - Generic script
+
+    Method called:
+    - Changes state of entity 'person.yongshi'
+
+    Test this class
+    """
+
+    # Next, we will define our initialize function, which is how AppDaemon starts our app. 
+    def initialize(self):
+
+        # restrict running only when you are called
+
+    def run(self):
+
+        if new == "new":
+            self.call_service("light/turn_on", entity_id = "light.arilux_rgb_led_controller", brightness = 255)
+        
+            # Now let's print a message when this function is called. Add the following line
+            self.log(f"Hey, Generic script ran!")
+            self.log(f"The state of entity {entity} changed from {old} to {new}!")
+
+        else:
+            self.call_service("light/turn_off", entity_id = "light.arilux_rgb_led_controller")
