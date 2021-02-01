3390c10f1cdc75bba9848283983ddfdd00e96b39
Michael-Yongshi <39827427+Michael-Yongshi@users.noreply.github.com>
2021-02-01 23:05:45 +0100

    renamed folder to seperate listners from pollers

---
+++ pollers/poll_smart_meter.py
@@ -0,0 +1,11 @@
+# PySmartMeter
+
+# https://computertotaal.nl/artikelen/overige-elektronica/slimme-meter-uitlezen-met-je-raspberry-pi/
+
+# https://www.domoticz.com/wiki/Raspberry_Pi
+
+# https://www.domoticz.com/wiki/Dutch_DSMR_smart_meter_with_P1_port
+
+# https://www.home-assistant.io/integrations/dsmr/
+# https://github.com/ndokter/dsmr_parser
+# https://www.jpaul.me/2019/01/how-to-build-a-raspberry-pi-serial-console-server-with-ser2net/
