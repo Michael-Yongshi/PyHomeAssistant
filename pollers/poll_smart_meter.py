f52b4679a2342e2731412356cfebf92b904e3948
Michael-Yongshi <39827427+Michael-Yongshi@users.noreply.github.com>
2021-02-02 16:08:20 +0100

    smart meter update

--- pollers/poll_smart_meter.py
+++ pollers/poll_smart_meter.py
@@ -1,37 +1,25 @@
 # PySmartMeter
 
-# https://computertotaal.nl/artikelen/overige-elektronica/slimme-meter-uitlezen-met-je-raspberry-pi/
-
-# https://www.domoticz.com/wiki/Raspberry_Pi
-
-# https://www.domoticz.com/wiki/Dutch_DSMR_smart_meter_with_P1_port
-
-# https://www.home-assistant.io/integrations/dsmr/
-# https://www.jpaul.me/2019/01/how-to-build-a-raspberry-pi-serial-console-server-with-ser2net/
+## use dedicated p1 smart meter pi zero to convert serial data and open to polling by home assistant
 
 
 
 
-#### Pull event from Home Assistant
-# https://www.home-assistant.io/integrations/rest/
+### Home assistant
+# Example configuration.yaml entry for remote (TCP/IP, i.e., via ser2net) connection to host which is connected to Smartmeter
 
-# The Rest sensor
-# Home Assistant P1 addon to poll ser2net on the pi connected to the smart meter.
+sensor:
+  - platform: dsmr
+    host: 192.168.1.13
+    port: 2001
+    dsmr_version: 5
 
-
-
-### reading p1 data on pi
-# https://github.com/ndokter/dsmr_parser
-pip3 install dsmr_parser
-
-from dsmr_parser import telegram_specifications
-from dsmr_parser.clients import SerialReader, SERIAL_SETTINGS_V4
-
-serial_reader = SerialReader(
-    device='/dev/ttyUSB0',
-    serial_settings=SERIAL_SETTINGS_V4,
-    telegram_specification=telegram_specifications.V4
-)
-
-for telegram in serial_reader.read():
-    print(telegram)  # see 'Telegram object' docs below
+group:
+  meter_readings:
+    name: Meter readings
+    entities:
+      - sensor.energy_consumption_tarif_1
+      - sensor.energy_consumption_tarif_2
+      - sensor.energy_production_tarif_1
+      - sensor.energy_production_tarif_2
+      - sensor.gas_consumption
