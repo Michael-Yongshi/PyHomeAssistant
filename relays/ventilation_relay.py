9b498b80f88c69f468fa2c6a4326070b64e6c163
Michael-Yongshi <39827427+Michael-Yongshi@users.noreply.github.com>
2021-02-04 21:41:50 +0100

    added ventilation relay

---
+++ relays/ventilation_relay.py
@@ -0,0 +1,5 @@
+https://steffankarger.nl/ventilation-control-using-a-pi-and-home-assistant.html
+
+post api ventilation
+expection integer of 1 - 3 for the speeds
+the rpi activates the different wires based on the inputted speed.
