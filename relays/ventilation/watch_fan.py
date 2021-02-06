988cff641f67d680499e6d2736d1141e2982293f
Michael-Yongshi <39827427+Michael-Yongshi@users.noreply.github.com>
2021-02-06 21:49:31 +0100

    added smoke

--- relays/ventilation/watch_fan.py
+++ relays/ventilation/watch_fan.py
@@ -22,32 +22,14 @@ class WatchFan(hass.Hass):
 
         # set override variable to default at initialization
         self.override = 0
-        
+        self.humidity = 0
+        self.smoke = 0
+
         # tells appdaemon we want to call a certain method when a certain event ("EVENT") is received. 
         self.listen_event(self.fan_override, "FAN_OVERRIDE")
         self.listen_event(self.humidity, "HUMIDITY")
+        self.listen_event(self.smoke, "SMOKE")
 
-    def humidity(self, event_name, data, kwargs):
-        
-        level = data["level"]
-        date_time = data["time"][0:18]
-        
-        # log the message
-        message = f"Measured humidity at level {level}! \nCurrent date and time is: {date_time}"
-        self.log(message)
-
-        if self.override == 0:
-            if level >= 700:
-                self.log(f"level 700 or higher observed")
-                # request speed 3
-
-            elif level >= 500:
-                # request speed 2
-                self.log(f"level between 500 and 700 observed")
-
-            else:
-                # request_speed 1
-                self.log(f"level 500 or lower observed")
 
     # the method that is called when someone wants to override fan setting
     def fan_override(self, event_name, data, kwargs):
@@ -62,3 +44,43 @@ class WatchFan(hass.Hass):
         # Call rest api of ventilation_api.pi on /override
         # add speed in data
         # if speed is 0 stop override
+
+    def humidity(self, event_name, data, kwargs):
+        
+        self.humidity = data["level"]
+        date_time = data["time"][0:18]
+        
+        # log the message
+        message = f"Measured humidity at level {self.humidity}! \nCurrent date and time is: {date_time}"
+        self.log(message)
+
+        if self.override == 0:
+
+
+
+        
+    def smoke(self, event_name, data, kwargs):
+        
+        level = data["level"]
+        date_time = data["time"][0:18]
+        
+        # log the message
+        message = f"Measured smoke at level {level}! \nCurrent date and time is: {date_time}"
+        self.log(message)
+
+        self.override = 
+
+    def determine_setting():
+
+        if self.smoke <= 500:
+            if self.humidity >= 700:
+                self.log(f"level 700 or higher observed")
+                # request speed 3
+
+            elif level >= 500:
+                # request speed 2
+                self.log(f"level between 500 and 700 observed")
+
+            else:
+                # request_speed 1
+                self.log(f"level 500 or lower observed")
