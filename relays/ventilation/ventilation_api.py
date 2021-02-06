6ddc7bfcac628e8c41acaa8c1664025ac4d90231
Michael-Yongshi <39827427+Michael-Yongshi@users.noreply.github.com>
2021-02-06 17:14:25 +0100

    moved and split in api part and generic method part

--- relays/ventilation/ventilation_api.py
+++ relays/ventilation/ventilation_api.py
@@ -8,69 +8,9 @@
 
 import os
 import json # to convert python list and dicts to json strings
-import time
-
-import RPi.GPIO as GPIO
+from ventilation import set_speed, read_speed
 
 from flask import Flask, request
-
-# set gpio wires
-wire1 = 16
-wire2 = 18
-
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(wire1, GPIO.OUT)
-GPIO.setup(wire2, GPIO.OUT)
-
-
-def set_speed(speed):
-    """
-    Code to set the speed of the fan by setting the two relays through GPIO output wires in a certain combination
-    
-    Both wires off means setting 1
-    Only wire 1 on means setting 2
-    Only wire 2 on means setting 3
-
-    both wires is ???
-    """
-
-    if speed == 1:
-        GPIO.output(wire1, False)
-        GPIO.output(wire2, False)
-
-    elif speed == 2:
-        GPIO.output(wire1, True)
-        GPIO.output(wire2, False)
-
-    elif speed == 3:
-        GPIO.output(wire1, False)
-        GPIO.output(wire2, True)
-    
-    else:
-        pass
-
-def read_speed():
-    """
-    Checks the wires on GPIO to see which setting is currently active
-
-    Both wires off means setting 1
-    Only wire 1 on means setting 2
-    Only wire 2 on means setting 3
-
-    if for some reason both wires are on, it will return setting 3
-    """
-
-    if GPIO.read(wire2) == True:
-        speed = 3
-
-    elif GPIO.read(wire1) == True:
-        speed = 2
-
-    else:
-        speed = 1
-    
-    return speed
-
 
 # run flask
 app = Flask(__name__)
