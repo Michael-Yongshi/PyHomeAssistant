b5aeba1ddcda60d53b400303f5b751fda5a674fc
Michael-Yongshi <39827427+Michael-Yongshi@users.noreply.github.com>
2021-02-28 09:45:45 +0100

    fixed override

--- fan_control/watch_fan.py
+++ fan_control/watch_fan.py
@@ -40,34 +40,62 @@ class WatchFan(hass.Hass):
         self.post_fan_speed(speed)
 
         # log
-        self.log(f"Someone requested fan override, setting speed {old} => {new}! \nCurrent date and time is: {self.override}")
+        self.log(f"Someone requested fan override, setting speed {old} => {new}! \nCurrent date and time is: {self.override}\n")
 
     def humidity(self, entity, attribute, old, new, kwargs):
-        
-        current_time = datetime.datetime.now()
-        
-        # log the message
-        self.log(f"Measured humidity at level {new}! \nCurrent date and time is: {current_time}")
+
+        # log humidity level
+        self.log(f"Measured humidity at {new}%!\n")
         humidity_new = int(new.split('.', 1)[0])
-        humidity_old = int(old.split('.', 1)[0])
+
+        # get fanspeed state
+        speed = int(self.get_state("sensor.mqtt_fan_speed"))
+        self.log(f"Fan speed is {speed}")
+
+        # timings
+        override_expiration = self.override + datetime.timedelta(minutes=15)
+        current_time = datetime.datetime.now()
+        self.log(f"Override expires at {override_expiration}\n")
+        self.log(f"Current date and time is: {current_time}")
 
         # automatically adjust fan speed based on sensor data, humidity and smoke
-        if self.override + datetime.timedelta(minutes=15) <= current_time:
+        if override_expiration <= current_time:
+            self.log(f"Override expired")
 
             # set to 3 if increased to above 80 from below 80 (2)
-            if humidity_new >= 80 and humidity_old < 80:
+            if humidity_new >= 80 and speed != 3:
                 self.post_fan_speed(3)
                 self.log(f"level above 80%  observed, set fan to speed 3")
 
             # set to 2 if humidity_new is between 60 - 80 (2) and humidity_old was below 60 (1) or above 80 (3)
-            elif (humidity_new >= 60 and humidity_new < 80) and (humidity_old < 60 or humidity_old >= 80):
+            elif (humidity_new >= 60 and humidity_new < 80) and speed != 2:
                 self.post_fan_speed(2)
                 self.log(f"level between 60% and 80% observed, set fan to speed 2")
 
             # set to 1 if new dropped below 60 (1) while humidity_old was higher than 60 (2 or 3) 
-            elif humidity_new < 60 and humidity_old >= 60:
+            elif humidity_new < 60 and speed != 1:
                 self.post_fan_speed(1)
                 self.log(f"level below 60% observed, set fan to speed 1")
+
+    def get_fan_speed(self):
+
+        # address for the rest api
+        url = "http://192.168.178.29:5000/get_speed"
+
+        # # denote that we are sending data in the form of a json string
+        # headers = {
+        #     "content-type": "application/json",
+        # }
+
+        # json = {
+        #     "speed": f"{speed}",
+        # }
+
+        # send out the actual request to the api
+        response = requests.get(url=url)
+        speed = response.text
+
+        return speed
 
     def post_fan_speed(self, speed):
         
