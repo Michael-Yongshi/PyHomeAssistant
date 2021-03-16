723f1e94da7e5248ec628b22d9b3ea28b36dcb51
Michael-Yongshi <39827427+Michael-Yongshi@users.noreply.github.com>
2021-03-16 14:23:53 +0100

    added '9' to turn off override and 10 min extending

--- fan_control/watch_fan.py
+++ fan_control/watch_fan.py
@@ -19,53 +19,79 @@ class WatchFan(hass.Hass):
 
     # Next, we will define our initialize function, which is how AppDaemon starts our app. 
     def initialize(self):
-
+        
         # Were keeping track of an override variable to keep override only on for a certain amount of time
-        self.override = datetime.datetime.now() + datetime.timedelta(minutes=-30)
+        self.override_expiration = datetime.datetime.now() + datetime.timedelta(minutes=-30)
+        self.override_interval = 10
 
         # tells appdaemon we want to call a certain method when a certain event ("EVENT") is received. 
-        self.listen_state(self.fan_override, "input_number.fan_override")
+        self.listen_event(self.fan_override, "FAN_OVERRIDE")
         self.listen_state(self.humidity, "sensor.mqtt_bathroom_humidity")
 
+        # enforce determining setting even if humidity is unchanged every minute
+        self.run_minutely(self.determine_setting, datetime.time(0, 0, 0))
+
     # the method that is called when someone wants to override fan setting from home assistant itself
-    def fan_override(self, entity, attribute, old, new, kwargs):
+    def fan_override(self, event_name, data, kwargs):
 
-        # remember last time override has been used
-        self.override = datetime.datetime.now()
+        speed = int(data["speed"])
+        oldspeed = int(self.get_fan_speed())
 
-        # convert the state to a valid integer
-        speed = int(new.split('.', 1)[0])
-        
-        # send fan command to set the speed to the new level
-        self.post_fan_speed(speed)
+        current_time = datetime.datetime.now()
 
-        # log
-        self.log(f"Someone requested fan override, setting speed {old} => {new}!")
-        self.log(f"Current date and time is: {self.override}")
+        # check if override is requested to be disable
+        if speed == 9:
+            # disable override by setting expiration to current time
+            self.override_expiration = current_time
+            self.log(f"Stopping fan override!")
+
+        # if override is active (and speed is the same as previous override) extend the override
+        elif speed == oldspeed and self.override_expiration > current_time:
+
+            # extend the override parameter
+            self.override_expiration += datetime.timedelta(minutes=self.override_interval)
+            
+            # log
+            self.log(f"Someone requested fan override, extending the override by {self.override_interval} minutes!")
+
+        else:
+            # if override is currently not active (for this speed) override is set anew
+            self.override_expiration = current_time + datetime.timedelta(minutes=self.override_interval)
+           
+            # send fan command to set the speed to the new level
+            self.post_fan_speed(speed)
+
+            # log
+            self.log(f"Someone requested fan override, setting speed {oldspeed} => {speed}!")
+
+        self.log(f"Current date and time is: {current_time}")
         self.log("")
 
+    # determine setting when humidity changed
     def humidity(self, entity, attribute, old, new, kwargs):
 
-        # log humidity level
-        self.log(f"Measured humidity at {new}%!")
-        humidity_level = int(new.split('.', 1)[0])
+        self.determine_setting(kwargs)
+
+    # determining the setting for the fan based on humidity and override expiration
+    def determine_setting(self, kwargs):
 
         # get fanspeed state
         speed = int(self.get_state("sensor.mqtt_fan_speed"))
         self.log(f"Fan speed is {speed}")
 
-        # timings
-        override_expiration = self.override + datetime.timedelta(minutes=15)
         current_time = datetime.datetime.now()
-        self.log(f"Override expires at {override_expiration}")
-        self.log(f"Current date and time is: {current_time}")
 
         limit3 = 95
         limit2 = 85
 
         # automatically adjust fan speed based on sensor data, humidity and smoke
-        if override_expiration <= current_time:
+        if self.override_expiration <= current_time:
             self.log(f"Override expired")
+
+            # humidity level
+            humidity = self.get_state("sensor.mqtt_bathroom_humidity")
+            humidity_level = int(humidity.split('.', 1)[0])
+            self.log(f"Measured humidity at {humidity}%!")
 
             # set to 3 if humidity_level increased to above limit3
             if humidity_level >= limit3:
@@ -94,6 +120,10 @@ class WatchFan(hass.Hass):
                 else:
                     self.log(f"level below {limit2}% observed, fan already at speed {setting}")
 
+        else:
+            until = self.override_expiration - current_time
+            self.log(f"Override active, expires in {until} minutes")
+            
         self.log("")
 
     def get_fan_speed(self):
