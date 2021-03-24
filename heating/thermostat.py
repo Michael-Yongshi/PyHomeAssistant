a3d5642ec059e0d1ebe48d11668d377f550e301b
Michael-Yongshi <39827427+Michael-Yongshi@users.noreply.github.com>
2021-03-24 22:02:17 +0100

    DHT sensor works

--- heating/thermostat.py
+++ heating/thermostat.py
@@ -3,13 +3,13 @@ import json
 import time
 import datetime
 import logging
-from time import daylight
+from time import daylight, strptime
 
 # heater relay operates by gpio signal
 import gpiozero
 
-# temperature sensor communicates through 1wire protocol
-# import w1thermsensor
+# temperature sensor communicates through adafruit protocol
+import adafruit_dht
 
 """ TODO
 mqtt push temperature and heating status to home assistant
@@ -37,7 +37,21 @@ class Thermostat(object):
 
         print(self.days)
 
+        # initialize thermometer
+        self.thermometer = Thermometer()
+
+        # initialize heater
+        self.heater = Heater()
+
         logging.debug('Thermostat object is initiated')
+
+    def run(self):
+
+        # try:
+        while True:
+            self.process()
+        # except:
+        #     logging.critical("Thermostat stopped unexpectedly!")
 
     def process(self):
 
@@ -47,7 +61,7 @@ class Thermostat(object):
         logging.info(f"Loading program {current_program_number}")
 
         # load program
-        current_program = self.programs[str(current_program_number)]
+        current_program = self.programs[current_program_number]
         logging.debug(f"Program loaded as: \n{current_program}")
 
         # check current target temperature
@@ -55,37 +69,43 @@ class Thermostat(object):
         while target_temp == 0:
             
             # get current time
-            current_time = datetime.time.now()
+            current_time = datetime.datetime.now() # .strftime("%H:%M:%S")
+            current_year = current_time.year
+            current_month = current_time.month
+            current_day = current_time.day
 
             logging.info(f"Searching for active timeslot... ({current_time})")
             for timeslot in current_program:
 
                 # Convert timeslot 'end' to time type
-                timeslot_time = datetime.datetime(timeslot["end"], "%h-%m-%s")
+                time_from_string = datetime.datetime.strptime(timeslot["end"], "%H:%M:%S")
+                timeslot_end = time_from_string.replace(year=current_year, month=current_month, day=current_day)
+                # print(timeslot_end)
 
                 # check if current time still falls within this timeslot
-                if current_time <= timeslot_time:
+                if current_time <= timeslot_end:
                     logging.info(f"Active timeslot is {timeslot}")
+                    print(f"current timeslot = {timeslot}")
 
                     # set timeslot temperature as current target
                     target_temp = timeslot["temp"]
                     logging.info(f"Target temperature is {current_program_number}")
 
+                    break
+
             # if no target temperature is set, restart this progress until it is set
 
         # get current temperature
-        thermometer = Thermometer()
-        temp = thermometer.get_temp()
+        temp = self.thermometer.get_temp()
 
         # get heater status
-        heater = Heater()
-        status = heater.get_status()
+        status = self.heater.get_status()
 
         # if heater is on (1), turn off only when temperature reaches the upper bound
         if status == 1:
             if temp >= target_temp:
                 logging.info(f"Target temperature rose above upper bound")
-                heater.switch.off()
+                self.heater.switch.off()
                 logging.info(f"Turned off heater")
             else:
                 logging.info(f"Target temperature still below upper bound")
@@ -95,7 +115,7 @@ class Thermostat(object):
         if status == 0:
             if temp < target_temp - 1:
                 logging.info(f"Target temperature fell below lower bound")
-                heater.switch.on()
+                self.heater.switch.on()
                 logging.info(f"Turned on heater")
             else:
                 logging.info(f"Target temperature still within bounds")
@@ -103,31 +123,23 @@ class Thermostat(object):
         # delay for a while
         time.sleep(self.delay)
 
-    def load_json(filename):
+    def load_json(self, filename):
         """Load settings json"""
 
-        path = os.path.join("~")
+        path = os.path.expanduser('~')
 
         # check if directory already exists, if not create it
         if not os.path.exists(path):
             print(f"cant find path '{path}'")
 
         else:
-            complete_path = os.path.join(path, 'filename' + ".json")
+            complete_path = os.path.join(path, filename + ".json")
 
             # open json and return as an array
             with open(complete_path, 'r') as infile:
                 contents = json.load(infile)
         
             return contents
-
-    def run(self):
-
-        try:
-            while True:
-                self.process()
-        except:
-            logging.critical("Thermostat stopped unexpectedly!")
 
 class Thermometer(object):
     """
@@ -137,24 +149,50 @@ class Thermometer(object):
 
     def __init__(self):
 
-        self.channelTemp = gpiozero.InputDevice(pin=4)
+        self.sensor = adafruit_dht.DHT22(pin=4)
 
         logging.debug('Thermometer object is initiated')
 
-    # def get_temp(self):
-    # https://bigl.es/ds18b20-temperature-sensor-with-python-raspberry-pi/
-    #     return self.channelTemp.value
+    def get_temp(self):
+
+        # DHR sensor has a tendency to have timing errors, so we retry on this specific error until a valid return is received
+        while True:
+            # try to get a valid read
+            try:
+                temperature = self.sensor.temperature
+
+            # continue in the while loop if an error is returned
+            except RuntimeError:
+                continue
+
+            # Breaks if code reaches this part, so only if try succeeded
+            break
+
+        # print(f"Temperature is {temperature}")
+        logging.info(f"Read DHT sensor {temperature}")
+
+        return temperature
+
+    def get_humid(self):
+
+        humidity = self.sensor.humidity
+
+        print(f"humidity {humidity}")
+        logging.info(f"Read DHT sensor {humidity}")
+
+        return humidity
 
 class Heater(object):
     """
-    The heater 
+    Heating is basically an on/off relay
+    Used for heaters that only switch on and off instead of the newer modulating relays
+
+    The self.switch variable contains a on() and off() method to manipulate the heater.
+    so calling 'heater.switch.on()' turns the heater on.
+
     """
 
     def __init__(self):
-        """
-        Heating is basically an on/off relay
-        Used for heaters that only switch on and off instead of the newer modulating relays
-        """
         
         self.switch = gpiozero.OutputDevice(pin=20, active_high=False)
 
@@ -177,25 +215,33 @@ class Heater(object):
         except:
             logging.error(f"Couldn't check relays!")
 
-    def get_heating(self):
+    def get_status(self):
         """
-        Check the current status of the heatings and returns result
-        if relay = 1, then heating is on
+        Check the current status of the heater and returns result
+        if relay = 1, then heater is on
         """
 
         try:
-            heating = self.switch.value
-            logging.info(f"Read heating {heating}")
-            return heating
+            status = self.switch.value
+
+            print(f"Heater status is {status}")
+            logging.info(f"Read heater {status}")
+            return status
 
         except:
-            logging.error(f"Couldn't read heating!")
+            logging.error(f"Couldn't read heater!")
             return -1
 
 if __name__ == '__main__':
     
-    heater = Heater()
+    # heater = Heater()
+    # heater.switch.on()
+    # heater.get_status()
+    # time.sleep(1)
+    # heater.switch.off()
 
-    heater.test_relay()
+    # thermometer = Thermometer()
+    # thermometer.get_temp()
 
-    # thermostat = Thermostat()
+    thermostat = Thermostat()
+    thermostat.run()
