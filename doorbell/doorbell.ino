6fea8663425d2dd3d30dced8b1ebf4568c563166
Michael-Yongshi <39827427+Michael-Yongshi@users.noreply.github.com>
2022-04-17 12:34:19 +0200

    working huzzah wifi led blink

--- doorbell/doorbell.ino
+++
@@ -1,6 +0,0 @@
-"""
-MQTT to send status ringing to home assistant
-"""
-
-on trigger, play sound on music maker featherwing + send mqtt change
-
---
+++ doorbell/local_doorbell/doorbell/secrets.h
@@ -0,0 +1,6 @@
+#define SECRET_WIFI_SSID "The Serenity"
+#define SECRET_WIFI_PASS "AlpacaDwaakC0nnected"
+
+#define SECRET_MQTT_BROKER "192.168.178.37"
+#define SECRET_MQTT_USER "mqttpublisher"
+#define SECRET_MQTT_PASS "publishmqtt"
