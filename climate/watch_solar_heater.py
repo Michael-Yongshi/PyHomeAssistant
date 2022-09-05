93bec2bab78d225a8ad50d21d7585316ee93fbc4
Michael-Yongshi <39827427+Michael-Yongshi@users.noreply.github.com>
2022-09-05 11:53:24 +0200

    changed to just use valley hours

--- climate/watch_solar_heater.py
+++ climate/watch_solar_heater.py
@@ -24,7 +24,17 @@ class WatchSolarHeater(hass.Hass):
         self.rolling_checks = ["off","off","off"]
 
         # loop method to determine if target temp needs to change
-        self.run_minutely(self.determine_setting, datetime.time(0, 0, 0))
+        # self.run_minutely(self.determine_setting, datetime.time(0, 0, 0))
+        self.run_daily(self.heater_on, datetime.time(23, 0, 0))
+        self.run_daily(self.heater_off, datetime.time(7, 0, 0))
+
+    def heater_on(self, kwargs):
+        self.call_service("climate/set_hvac_mode", entity_id=self.climate_entity, hvac_mode="heat")
+        self.event_happened(f"Turned on electric heater")
+
+    def heater_off(self, kwargs):
+        self.call_service("climate/set_hvac_mode", entity_id=self.climate_entity, hvac_mode="off")
+        self.event_happened(f"Turned off electric heater")
 
     def determine_setting(self, kwargs):
         """
@@ -32,38 +42,43 @@ class WatchSolarHeater(hass.Hass):
         its assumed the thermostat itself arranges the actual turning on of the heater
         """
 
-        # Add a rolling check if enough solar energy or too little
-        solar_yield = int(self.get_state(self.solar_yield_entity))
-        self.event_happened(f"solar yield is {solar_yield}")
+        self.check_solar_yield(self)
 
-        if solar_yield >= 200:
-
-            self.rolling_checks = self.rolling_checks[1:] + ["heat"]
-            self.event_happened(f"New rolling checks: {self.rolling_checks}")
-
-        else:
-            self.rolling_checks = self.rolling_checks[1:] + ["off"]
-            self.event_happened(f"New rolling checks: {self.rolling_checks}")
-
+        # TODO
         # # Get status of the climate entity itself (not the action, this can differ depending on temperature)
         # heater_status = self.get_state(self.climate_entity, attribute="operation_mode")
         # self.event_happened(f"heater status = {heater_status}")
 
-        # Turn on if solar energy is enough and heater is still off
+        # Turn on if rolling checks are satisfied and heater is still off
         if self.rolling_checks == ["heat", "heat", "heat"]:
         # if self.rolling_checks == ["heat", "heat", "heat"] and heater_status == "off":
-            self.call_service("climate/set_hvac_mode", entity_id=self.climate_entity, hvac_mode="heat")
-            self.event_happened(f"Turned on electric heater, solar yield = {solar_yield}")
+            self.heater_on()
 
         # vice versa
         # elif self.rolling_checks == ["off", "off", "off"] and heater_status == "heat":
         elif self.rolling_checks == ["off", "off", "off"]:
-            self.call_service("climate/set_hvac_mode", entity_id=self.climate_entity, hvac_mode="off")
-            self.event_happened(f"Turned off electric heater, solar yield = {solar_yield}")
+            self.heater_off()
 
         # ignore if not stable reading
         else:
             pass
+
+    def check_solar_yield(self):
+        """
+        Add a rolling check if enough solar energy or too little
+        """
+
+        solar_yield = int(self.get_state(self.solar_yield_entity))
+        # self.event_happened(f"solar yield is {solar_yield}")
+
+        if solar_yield >= 200:
+
+            self.rolling_checks = self.rolling_checks[1:] + ["heat"]
+            # self.event_happened(f"New rolling checks: {self.rolling_checks}")
+
+        else:
+            self.rolling_checks = self.rolling_checks[1:] + ["off"]
+            # self.event_happened(f"New rolling checks: {self.rolling_checks}")
 
     def event_happened(self, message):
         """
