5512250012c688fd27540f74a10323d8e9988b2b
Yongshi <4registration@outlook.com>
2023-11-28 15:06:19 +0100

    removed Lights and Fan to own Repo

--- fan/fan_arduino/secrets.h
+++
@@ -1,6 +0,0 @@
-#define SECRET_WIFI_SSID "The Serenity"
-#define SECRET_WIFI_PASS "AlpacaDwaakC0nnected"
-
-#define SECRET_MQTT_BROKER "192.168.178.37"
-#define SECRET_MQTT_USER "mqttpublisher"
-#define SECRET_MQTT_PASS "publishmqtt"
